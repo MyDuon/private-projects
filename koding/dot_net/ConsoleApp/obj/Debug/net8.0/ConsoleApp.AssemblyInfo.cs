8bd06927c999c8b6c0787a7d9812452ecb070c3d
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-01-17 14:19:25 +0800

    clearing up data

--- koding/dot_net/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ koding/dot_net/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+943fb4dc92a121f332d76aab3d97ebb92b6e38c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
