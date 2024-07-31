e32c19bb7f273e76c02bb4b0c6e296801cbbc707
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-07-31 14:06:48 +0200

    added dummy input and button

--- koding/javascript/todo-list/src/App.jsx
+++ koding/javascript/todo-list/src/App.jsx
@@ -5,7 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>test</div>
+    <div>
+        <h1>TODO-LIST</h1>
+        <input name="myInput" />
+        <button>Add</button>
+    </div>
   )
 }
 
