2efda1189c5eaf45ec5396b77ef44ccca1d39e20
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-07-31 14:28:10 +0200

    header with different section

--- koding/javascript/todo-list/src/App.jsx
+++ koding/javascript/todo-list/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+        <Header/>
         <h1>TODO-LIST</h1>
         <input name="myInput" />
         <button>Add</button>
