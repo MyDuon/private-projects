e1085056ad1f584f443b63d10237a05e1f22b647
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-07-31 14:58:24 +0200

    added icon

--- koding/javascript/todo-list/src/App.jsx
+++ koding/javascript/todo-list/src/App.jsx
@@ -1,15 +1,14 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-        <Header/>
-        <h1>TODO-LIST</h1>
-        <input name="myInput" />
-        <button>Add</button>
-    </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <MainPart/>
+            <Footer/>
+        </div>
+    )
 }
-
 export default App
---
+++ koding/javascript/todo-list/src/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer () {
+    return (
+        <footer>
+            <p>&copy; - {new Date().getDate()}.{new Date().getMonth()}.{new Date().getFullYear()} TODO-list made by My Hoang Duong</p>
+        </footer>
+    )
+}
+export default Footer
