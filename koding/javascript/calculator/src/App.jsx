e1bc09e2ab73ae867e43ac41484d5dc2ec6f347c
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-08-11 21:09:11 +0200

    fixed reset button

--- koding/javascript/calculator/src/App.jsx
+++ koding/javascript/calculator/src/App.jsx
@@ -28,6 +28,10 @@ function App() {
     setCalc(tmp);
   };
 
+  const reset = () => {
+    setCalc("0");
+  };
+
   const operation = ["+", "-", "*", "/", "."];
   const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -88,7 +92,11 @@ function App() {
             <Button variant="contained" onClick={() => calculate(calc)}>
               =
             </Button>
-            <Button className="clear-entry" variant="contained">
+            <Button
+              className="clear-entry"
+              onClick={() => reset()}
+              variant="contained"
+            >
               CE
             </Button>
           </Stack>
