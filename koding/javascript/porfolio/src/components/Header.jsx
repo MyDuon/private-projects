7858addc51d7f5bd47cccfcdfc20195afbd3d397
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-09-03 11:35:42 +0200

    removed all css

--- koding/javascript/porfolio/src/components/Header.jsx
+++ koding/javascript/porfolio/src/components/Header.jsx
@@ -1,6 +1,10 @@
 function Header() {
   return (
     <>
+      <div className="header-container">
+        <p>Home</p>
+        <div>test</div>
+      </div>
       <h1>Portfolio</h1>
       <p>
         DETTE er en header. Bruk navbar til a navigere eller npm react scroll
