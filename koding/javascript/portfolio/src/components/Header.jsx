961e9880fea5483afde5e3d558e97cf4a7c513dd
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-09-04 15:55:18 +0200

    startet on navbar

--- koding/javascript/portfolio/src/components/Header.jsx
+++ koding/javascript/portfolio/src/components/Header.jsx
@@ -1,15 +1,18 @@
 function Header() {
   return (
     <>
-      <div className="header-container">
-        <p>Home</p>
-        <div>test</div>
-      </div>
-      <h1>Portfolio</h1>
-      <p>
-        DETTE er en header. Bruk navbar til a navigere eller npm react scroll
-        bibliotek
-      </p>
+      <header className="header">
+        <a href="" className="logo">
+          Ola Nordmann
+        </a>
+        <nav className="nav">
+          <a href="/">About</a>
+          <a href="/">Projects</a>
+          <a href="/">Resume</a>
+          <a href="/">Experience</a>
+          <a href="/">Contact</a>
+        </nav>
+      </header>
     </>
   );
 }
