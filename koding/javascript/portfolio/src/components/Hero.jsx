ce0710a5cdd7700222be1106010bb97c0fef12d0
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-11-06 16:42:00 +0100

    fiksa bug

--- koding/javascript/portfolio/src/components/Hero.jsx
+++ koding/javascript/portfolio/src/components/Hero.jsx
@@ -4,22 +4,20 @@ function Hero() {
   return (
     <>
       <div className="hero-wrapper">
-<<<<<<< HEAD
         <div className="description">
           Hei, Jeg er My Hoang Duong. Fullstack utvikler
         </div>
         <div className="hero-picture">
-=======
-        <div className="left-hero">
-          <h1 className="description">Hei, Jeg er My</h1>
-          <h3>
-            Informatikk student som er på leting etter en utvikler jobb, enten
-            fullstack, frontend eller backend
-          </h3>
-        </div>
-        <div className="right-hero">
->>>>>>> 81f9d39 (testing)
-          <img src={picture} alt="picture of me" />
+          <div className="left-hero">
+            <h1 className="description">Hei, Jeg er My</h1>
+            <h3>
+              Informatikk student som er på leting etter en utvikler jobb, enten
+              fullstack, frontend eller backend
+            </h3>
+          </div>
+          <div className="right-hero">
+            <img src={picture} alt="picture of me" />
+          </div>
         </div>
       </div>
     </>
