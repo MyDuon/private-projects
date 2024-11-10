f9b0c7c34010fc953306c39e020c2146b4b7e920
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-11-10 20:09:17 +0100

    fixed size on experience

--- koding/javascript/portfolio/src/components/SideProject.jsx
+++ koding/javascript/portfolio/src/components/SideProject.jsx
@@ -2,16 +2,18 @@ import "./SideProject.css";
 function SideProject() {
   return (
     <>
-      <h1 className="project-header">Project</h1>
-      <div className="side-project">
-        <div>Streamingtjeneste</div>
-        <div>Calculator</div>
-        <div>3-paa-rad</div>
-      </div>
-      <div className="side-project">
-        <div>todo-list</div>
-        <div>test</div>
-        <div>test</div>
+      <div className="side-project-wrapper">
+        <h1 className="project-header">Project</h1>
+        <div className="side-project">
+          <div>Streamingtjeneste</div>
+          <div>Calculator</div>
+          <div>3-paa-rad</div>
+        </div>
+        <div className="side-project">
+          <div>todo-list</div>
+          <div>test</div>
+          <div>test</div>
+        </div>
       </div>
     </>
   );
