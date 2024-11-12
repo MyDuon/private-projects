a8ad384c202d7bf3e50491a3f85f19e137bab9be
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-11-12 12:25:32 +0100

    added font

--- koding/javascript/portfolio/src/App.jsx
+++ koding/javascript/portfolio/src/App.jsx
@@ -1,3 +1,7 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./index.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
