930216d999ee0795108a9e46641b6c4701e875a4
MyDuon <100708123+MyDuon@users.noreply.github.com>
2024-11-14 15:58:37 +0100

    added more margin

--- koding/javascript/portfolio/src/components/Experience.jsx
+++ koding/javascript/portfolio/src/components/Experience.jsx
@@ -11,17 +11,17 @@ function Experience() {
           <div className="uio-wrapper">
             <UioLogo />
             <h2>Universitet i Oslo</h2>
-            <h3>Gruppelærer</h3>
+            <h3 className="margin">Gruppelærer</h3>
             <p className="margin">
-              Gjennomføre seminartimer med studenter, rettet obligatoriske
-              oppgaver og ga tilbakemelding. Hadde ukentlig møter med faglærer
-              og andre gruppelærere.
+              Hadde seminartimer med studenter, rettet obligatoriske oppgaver og
+              ga tilbakemelding. Hadde ukentlig møter med faglærer og
+              gruppelærere.
             </p>
           </div>
           <div className="munch-wrapper">
             <MunchLogo />
             <h2>Munch</h2>
-            <h3>Vertskap</h3>
+            <h3 className="margin">Vertskap</h3>
             <p className="margin">
               Selger billetter, audioguide, står i utstillingene. Hjelper med
               henvendelser og spørsmål fra kunden.
